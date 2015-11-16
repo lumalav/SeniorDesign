c453e0fcffda1e178069bb97b65da7b8ff4122dd
Luis Lavieri <lumalav@gmail.com>
2015-11-16 00:18:21 -0500

    First Commit
    Added MVC Project and capability of uploading big blobs to azure

---
+++ 3DCytoFlow/3DCytoFlow/Models/CloudFilesModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+
+namespace _3DCytoFlow.Models
+{
+    public class CloudFilesModel
+    {
+        public CloudFilesModel()
+            : this(null)
+        {
+            Files = new List<CloudFile>();
+        }
+
+        public CloudFilesModel(IEnumerable<IListBlobItem> list)
+        {
+            Files = new List<CloudFile>();
+            try
+            {
+                if (list != null)
+                {
+                    foreach (var item in list)
+                    {
+                        CloudFile info = CloudFile.CreateFromIListBlobItem(item);
+                        if (info != null)
+                        {
+                            Files.Add(info);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Ignore Errors when empty
+            }
+        }
+        public List<CloudFile> Files { get; set; }
+    }
+}
