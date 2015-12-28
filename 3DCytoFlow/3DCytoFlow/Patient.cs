5cc26866e804fc2c738d36ca2e203029d830db3c
Luis Lavieri <lumalav@gmail.com>
2015-12-28 11:57:31 -0500

    Added client side validation

--- 3DCytoFlow/3DCytoFlow/Patient.cs
+++ 3DCytoFlow/3DCytoFlow/Patient.cs
@@ -7,6 +7,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace _3DCytoFlow
 {
     using System;
@@ -20,16 +23,28 @@ namespace _3DCytoFlow
         }
     
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
         public string Middle { get; set; }
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
         public string DOB { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Invalid Phone Number")]
         public string Phone { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
+
+        [RegularExpression(@"^\d{5}(-\d{4})?$", ErrorMessage = "Invalid Zip")]
         public string Zip { get; set; }
-    
         public virtual User User { get; set; }
         public virtual ICollection<Analysis> Analyses { get; set; }
     }
