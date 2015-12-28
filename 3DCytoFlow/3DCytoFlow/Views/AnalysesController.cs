a76d56058fd559f1bc1217b1e7627d845f5558f1
Luis Lavieri <lumalav@gmail.com>
2015-12-28 11:12:37 -0500

    Added Patient and Analysis Controller

---
+++ 3DCytoFlow/3DCytoFlow/Views/AnalysesController.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace _3DCytoFlow.Views
+{
+    public class AnalysesController : Controller
+    {
+        private readonly x0033_DCytoFlowDBContainer _db = new x0033_DCytoFlowDBContainer();
+
+        // GET: Analyses
+        public ActionResult Index()
+        {
+            return View(_db.Analyses.ToList());
+        }
+
+        // GET: Analyses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var analysis = _db.Analyses.Find(id);
+            
+            if (analysis == null)
+            {
+                return HttpNotFound();
+            }
+            return View(analysis);
+        }
+
+        // GET: Analyses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+            var analysis = _db.Analyses.Find(id);
+            
+            if (analysis == null)
+            {
+                return HttpNotFound();
+            }
+            return View(analysis);
+        }
+
+        // POST: Analyses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var analysis = _db.Analyses.Find(id);
+            
+            _db.Analyses.Remove(analysis);
+            _db.SaveChanges();
+            
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ 3DCytoFlow/3DCytoFlow/Views/PatientsController.cs
@@ -0,0 +1,132 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace _3DCytoFlow.Views
+{
+    public class PatientsController : Controller
+    {
+        private readonly x0033_DCytoFlowDBContainer _db = new x0033_DCytoFlowDBContainer();
+
+        // GET: Patients
+        public ActionResult Index()
+        {
+            return View(_db.Patients.ToList());
+        }
+
+        // GET: Patients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var patient = _db.Patients.Find(id);
+            
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,Middle,LastName,DOB,Email,Phone,Address,City,Zip")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Patients.Add(patient);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(patient);
+        }
+
+        // GET: Patients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+            var patient = _db.Patients.Find(id);
+            
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,Middle,LastName,DOB,Email,Phone,Address,City,Zip")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(patient).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+            var patient = _db.Patients.Find(id);
+            
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            
+            var patient = _db.Patients.Find(id);
+            
+            _db.Patients.Remove(patient);
+            
+            _db.SaveChanges();
+            
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
